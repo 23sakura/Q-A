70818f6a06e31737a7b539c530cd677b17bb2cfb
Haruka11 <ushiromiya3@gmail.com>
2017-08-06 21:35:33 +0900

    complete

---
+++ app/src/main/java/com/example/haruka/rescue_aid/Display_qr.java
@@ -0,0 +1,25 @@
+package com.example.haruka.rescue_aid;
+
+import android.support.v4.app.FragmentActivity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+//import com.google.zxing.BarcodeFormat;
+//import com.google.zxing.common.BitMatrix;
+//import com.google.zxing.qrcode.QRCodeWriter;
+
+public class Display_qr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_qr);
+    }
+}
--- app/src/main/java/com/example/haruka/rescue_aid/InterviewActivity.java
+++ app/src/main/java/com/example/haruka/rescue_aid/InterviewActivity.java
@@ -1,13 +1,53 @@
 package com.example.haruka.rescue_aid;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InterviewActivity extends AppCompatActivity {
+
+    private Context context;
+    private Intent mToQr;
+    private Button mBtnYes;
+    private Button mBtnNo;
+    private TextView mInterviewContent;
+    private String[] contents = {"意識はありますか？", "息をしていますか？", "頭からの出血はありますか？", "周りに危険物はありますか？"};
+    private int offset = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interview);
+
+        context = this;
+        mToQr = new Intent(this, Display_qr.class);
+        mBtnYes = (Button)findViewById(R.id.btn_yes);
+        mBtnNo  = (Button)findViewById(R.id.btn_no);
+        mInterviewContent = (TextView)findViewById(R.id.interview);
+        mInterviewContent.setText(contents[offset]);
+
+
+        mBtnYes.setOnClickListener(interAnsBtnListener);
+        mBtnNo.setOnClickListener(interAnsBtnListener);
     }
+    View.OnClickListener interAnsBtnListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            if(++offset < contents.length ) mInterviewContent.setText(contents[offset]);
+            else {
+                new AlertDialog.Builder(context).setMessage("問診は終了です\nQRコードを表示します").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(mToQr);
+                    }
+                }).show();
+            }
+        }
+    };
 }
