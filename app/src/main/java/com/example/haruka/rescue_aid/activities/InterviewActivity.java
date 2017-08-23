b56f617da2a5dea04fb042ad9317bab2bdb89d4c
skriulle <skriulle5@gmail.com>
2017-08-24 00:37:37 +0900

    add voice recognition on Interview Activity

--- app/src/main/java/com/example/haruka/rescue_aid/activities/InterviewActivity.java
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/InterviewActivity.java
@@ -1,16 +1,25 @@
 package com.example.haruka.rescue_aid.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.haruka.rescue_aid.R;
+import com.example.haruka.rescue_aid.recognition_list.YesClass;
+
+import java.util.ArrayList;
 
 public class InterviewActivity extends AppCompatActivity {
 
@@ -23,6 +32,95 @@ public class InterviewActivity extends AppCompatActivity {
     private int offset = 0;
     private String mResult = "";
 
+    private ArrayList<String>[] dictionary;
+
+
+    class SpeechListener implements RecognitionListener {
+
+        @Override
+        public void onBeginningOfSpeech() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onError(int error) {
+            // TODO Auto-generated method stub
+            if(error == 9) {
+                //get Permission
+                ActivityCompat.requestPermissions(InterviewActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+            }else if (error != 7){
+                Toast.makeText(getApplicationContext(), "エラー " + error, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            // TODO Auto-generated method stub
+            Toast.makeText(getApplicationContext(), "認識開始", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // TODO Auto-generated method stub
+
+
+            int yes = 0;
+            ArrayList<String> candidates = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            for(yes = 0; yes < 2; yes++){
+                for(int index = 0; index < dictionary[yes].size(); index++){
+                    if(dictionary[yes].get(index).equals(candidates.get(0))){
+                        Toast.makeText(getApplicationContext(), (yes == 0) ?"Yes":"No" , Toast.LENGTH_SHORT).show();
+                        if(yes == 0){
+                            mBtnYes.callOnClick();
+                        }else{
+                            mBtnNo.callOnClick();
+                        }
+                        break;
+                    }
+                }
+            }
+
+            /*
+            ArrayList<String> candidates = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            */
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+    private SpeechRecognizer sr;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +128,23 @@ public class InterviewActivity extends AppCompatActivity {
 
         context = this;
         mToQr = new Intent(this, Display_qr.class);
-        mBtnYes = (Button)findViewById(R.id.btn_yes);
-        mBtnNo  = (Button)findViewById(R.id.btn_no);
-        mInterviewContent = (TextView)findViewById(R.id.interview);
+        mBtnYes = (Button) findViewById(R.id.btn_yes);
+        mBtnNo = (Button) findViewById(R.id.btn_no);
+        mInterviewContent = (TextView) findViewById(R.id.interview);
         mInterviewContent.setText(contents[offset]);
 
 
         mBtnYes.setOnClickListener(interAnsBtnListener);
         mBtnNo.setOnClickListener(interAnsBtnListener);
+
+        dictionary = YesClass.getDictionary();
+
+        sr = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+        sr.setRecognitionListener(new SpeechListener());
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        sr.startListening(intent);
     }
     View.OnClickListener interAnsBtnListener = new View.OnClickListener(){
         @Override
@@ -53,6 +160,10 @@ public class InterviewActivity extends AppCompatActivity {
             }
             if(++offset < contents.length) {
                 mInterviewContent.setText(contents[offset]);
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+                sr.startListening(intent);
             }else {
                 new AlertDialog.Builder(context).setMessage("問診は終了です\nQRコードを表示します").setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
