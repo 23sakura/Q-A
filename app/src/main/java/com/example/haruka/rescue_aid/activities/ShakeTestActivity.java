8f1b8f076c3b7da0d2422c70a86929d4438cd2fa
skriulle <skriulle5@gmail.com>
2017-08-24 13:22:56 +0900

    add test platform

---
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/ShakeTestActivity.java
@@ -0,0 +1,12 @@
+package com.example.haruka.rescue_aid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tomoya on 8/24/2017 AD.
+ */
+
+public class ShakeTestActivity extends AppCompatActivity{
+
+
+}
