dd21ac6c9289178f5ebb8d4ddc706d634e18a888
skriulle <skriulle5@gmail.com>
2017-08-26 23:44:31 +0900

    add CVCamera Test

---
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/CVCameraTestActivity.java
@@ -0,0 +1,10 @@
+package com.example.haruka.rescue_aid.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tomoya on 8/26/2017 AD.
+ */
+
+public class CVCameraTestActivity extends AppCompatActivity {
+}
