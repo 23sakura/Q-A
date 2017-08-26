0e256fff747c582dae6ef6cd336c90cc2c193a7b
skriulle <skriulle5@gmail.com>
2017-08-26 15:33:05 +0900

    fix camera orientation

--- app/src/main/java/com/example/haruka/rescue_aid/activities/TitleActivity.java
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/TitleActivity.java
@@ -2,44 +2,50 @@ package com.example.haruka.rescue_aid.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.haruka.rescue_aid.R;
 
+import org.opencv.android.OpenCVLoader;
+
 public class TitleActivity extends AppCompatActivity {
 
-    private Button mStartbtn, vRecognizeBtn;
-    private Intent mToInterview, mToVRecognition;
+    private Button gotoInterviewBtn, gotoTestBtn;
+    private Intent interviewIntent, testIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
 
-        mToInterview = new Intent(this, InterviewActivity.class);
-        mToVRecognition = new Intent(this, VoiceRecognizeActivity.class);
+        interviewIntent = new Intent(this, InterviewActivity.class);
+        testIntent = new Intent(this, TestPlatformActivity.class);
 
-        mStartbtn = (Button)findViewById(R.id.startbtn);
-        mStartbtn.setOnClickListener(new View.OnClickListener() {
+        gotoInterviewBtn = (Button)findViewById(R.id.startbtn);
+        gotoInterviewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(mToInterview);
+                startActivity(interviewIntent);
             }
         });
 
-        vRecognizeBtn = (Button)findViewById(R.id.voiceRecognizeBtn);
-        vRecognizeBtn.setOnClickListener(new View.OnClickListener(){
+        gotoTestBtn = (Button)findViewById(R.id.gotoTestBtn);
+        gotoTestBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                mToVRecognition.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                mToVRecognition.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getApplication().getPackageName());
-                mToVRecognition.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
-                startActivity(mToVRecognition);
-
+                startActivity(testIntent);
             }
         });
+
+
+        if (!OpenCVLoader.initDebug()){
+            Log.i("OpenCV", "Failed");
+        }else{
+            Log.i("OpenCV", "Successfully build!");
+        }
     }
 }
+
