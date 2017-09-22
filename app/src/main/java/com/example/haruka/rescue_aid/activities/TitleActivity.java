281432ceb18574253ff0d85f0db39d15efcd6638
skriulle <skriulle5@gmail.com>
2017-09-23 00:14:34 +0900

    add overlay on TitleActivity

--- app/src/main/java/com/example/haruka/rescue_aid/activities/TitleActivity.java
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/TitleActivity.java
@@ -1,9 +1,12 @@
 package com.example.haruka.rescue_aid.activities;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,15 +14,47 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.haruka.rescue_aid.R;
+import com.example.haruka.rescue_aid.views.CallOverlay;
 
 public class TitleActivity extends AppCompatActivity {
 
     private Button gotoInterviewBtn, gotoTestBtn, historyBtn;
-    private Intent interviewIntent, testIntent, qrIntent;
+    private Intent interviewIntent, testIntent, qrIntent, overlayIntent;
     private Context context;
+
+
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1000;
+
+    void closeOverLay(){
+        stopService(overlayIntent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                // SYSTEM_ALERT_WINDOW permission not granted...
+                // nothing to do !
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+        overlayIntent = new Intent(getApplication(), CallOverlay.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
         context = this;
@@ -55,6 +90,8 @@ public class TitleActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        CallOverlay.setText("ほげほげ");
     }
 
 
@@ -72,6 +109,7 @@ public class TitleActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.menu_call_119:
                 Uri uri = Uri.parse("tel:09052793706");
+                startService(overlayIntent);
                 Intent i = new Intent(Intent.ACTION_DIAL,uri);
                 startActivity(i);
                 break;
---
+++ app/src/main/java/com/example/haruka/rescue_aid/views/CallOverlay.java
@@ -0,0 +1,88 @@
+package com.example.haruka.rescue_aid.views;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.haruka.rescue_aid.R;
+
+/**
+ * Created by skriulle on 9/22/2017 AD.
+ */
+
+public class CallOverlay extends Service {
+
+    private View view;
+    private WindowManager windowManager;
+    private int dpScale ;
+
+    public static String text;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // dipを取得
+        dpScale = (int)getResources().getDisplayMetrics().density;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+
+        windowManager = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity=  Gravity.TOP | Gravity.RIGHT;
+
+        view = layoutInflater.inflate(R.layout.service_layer, null);
+
+        // Viewを画面上に追加
+        windowManager.addView(view, params);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //windowManager.removeView(v);
+                TextView textView = (TextView) ((LinearLayout)v).getChildAt(0);
+                textView.setText(text);
+            }
+        });
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public static void setText(String text){
+        CallOverlay.text = text;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("debug","onDestroy");
+        // Viewを削除
+        windowManager.removeView(view);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
