ca78f721d46553b65669d53dd34d986af2605071
skriulle <skriulle5@gmail.com>
2017-09-22 14:54:43 +0900

    add frame on QRActivity

--- app/src/main/java/com/example/haruka/rescue_aid/activities/QRActivity.java
+++ app/src/main/java/com/example/haruka/rescue_aid/activities/QRActivity.java
@@ -5,8 +5,12 @@ package com.example.haruka.rescue_aid.activities;
  */
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.hardware.Camera;
 import android.hardware.Camera.AutoFocusCallback;
 import android.os.Bundle;
@@ -18,6 +22,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Toast;
 
 import com.example.haruka.rescue_aid.utils.MedicalCertification;
@@ -42,6 +47,7 @@ public class QRActivity extends AppCompatActivity {
         mSurfaceView = new SurfaceView(this);
         mSurfaceView.setOnClickListener(onClickListener);
         setContentView(mSurfaceView);
+        addContentView(new CameraOverlayView(this), new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
     }
 
     @Override
@@ -54,9 +60,6 @@ public class QRActivity extends AppCompatActivity {
     private SurfaceHolder.Callback callback = new SurfaceHolder.Callback() {
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
-            //CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
-            //manager.openCamera(mCameraId, mStateCallback, mBackgroundHandler);
-
             int permissionCheck = ContextCompat.checkSelfPermission(QRActivity.this, Manifest.permission.CAMERA);
 
             if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
@@ -66,7 +69,7 @@ public class QRActivity extends AppCompatActivity {
 
             } else {
             }
-            mCamera = Camera.open(0);
+            mCamera = Camera.open();
             mCamera.lock();
             mCamera.setDisplayOrientation(90);
             try {
@@ -74,7 +77,20 @@ public class QRActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+/*
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            paint.setColor(Color.BLUE);
+            paint.setStyle(Paint.Style.FILL);
+
+            Canvas canvas = holder.lockCanvas();
+            //canvas.drawColor(Color.BLACK);
+            canvas.drawCircle(100, 200, 50, paint);
+            holder.unlockCanvasAndPost(canvas);
+*/
+
         }
+
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             Camera.Parameters parameters = mCamera.getParameters();
@@ -84,6 +100,7 @@ public class QRActivity extends AppCompatActivity {
             mCamera.setParameters(parameters);
             mCamera.startPreview();
         }
+
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
             mCamera.release();
@@ -109,7 +126,7 @@ public class QRActivity extends AppCompatActivity {
         }
     };
 
-    private void showQR(MedicalCertification medicalCertification){
+    private void showQR(MedicalCertification medicalCertification) {
         final Intent QRIntent = new Intent(this, QRDisplayActivity.class);
 
         QRIntent.putExtra("RESULT", medicalCertification.toString());
@@ -117,7 +134,7 @@ public class QRActivity extends AppCompatActivity {
         startActivity(QRIntent);
     }
 
-    private void showCertification(MedicalCertification medicalCertification){
+    private void showCertification(MedicalCertification medicalCertification) {
         final Intent QRIntent = new Intent(this, CertificationActivity.class);
 
         QRIntent.putExtra("CERTIFICATION", medicalCertification);
@@ -154,4 +171,45 @@ public class QRActivity extends AppCompatActivity {
         }
     };
 
+    class CameraOverlayView extends View {
+        private int width, height;
+
+        public CameraOverlayView(Context context) {
+            super(context);
+            setFocusable(true);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            // ビューのサイズを取得
+            width = w;
+            height = h;
+        }
+
+        /**
+         * 描画処理
+         */
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            // 背景色を設定
+            canvas.drawColor(Color.TRANSPARENT);
+
+            // 描画するための線の色を設定
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setARGB(100, 0, 0, 0);
+
+            // 上枠表示
+            canvas.drawRect(0, 0, (width - height) / 2, height, paint);
+            // 下枠表示
+            canvas.drawRect((width - height) / 2 + height, 0, width, height, paint);
+
+            // 円表示
+            paint.setARGB(255, 255, 0, 0);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(width / 14, height / 2-width/7*3,width / 14*13, height / 2+width/7*3, paint);
+        }
+
+    }
 }
