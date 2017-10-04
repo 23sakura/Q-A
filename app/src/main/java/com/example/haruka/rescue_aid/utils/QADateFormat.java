88745b89542d78e98f4da3845f3da3f4d8c494e1
skriulle <skriulle5@gmail.com>
2017-10-05 01:41:56 +0900

    add new format to resolve the problem with 24h

--- app/src/main/java/com/example/haruka/rescue_aid/utils/QADateFormat.java
+++ app/src/main/java/com/example/haruka/rescue_aid/utils/QADateFormat.java
@@ -1,7 +1,5 @@
 package com.example.haruka.rescue_aid.utils;
 
-import android.util.Log;
-
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,43 +13,61 @@ import java.util.Date;
 public class QADateFormat {
 
     public final static String simpleDateFormat =  "yyyy.MM.dd kk:mm:ss";
-    public final static String simpleDateFormat2 =  "yyyy.MM.dd kk.mm.ss";
+    public final static String simpleDateFormat_ =  "yyyy.MM.dd 00:mm:ss";
     public final static String JapaneseDateFormat =  "yyyy年MM月dd日 kk時mm分";
+    public final static String JapaneseDateFormat_ =  "yyyy年MM月dd日 00時mm分";
     public final static String FileDateFormat = "yyyyMMddkkmmss";
+    public final static String FileDateFormat_ = "yyyyMMdd00mmss";
     public final static String FileDateFormat2 = "MM/dd kk:mm";
+    public final static String FileDateFormat2_ = "MM/dd 00:mm";
+    private final static String DateFormatCheck = "kk";
+
+    public static boolean check00(Date date){
+        return new SimpleDateFormat(DateFormatCheck).format(date).equals("24");
+    }
 
     public static String getInstance(){
         Date date = new Date();
         return getStringDate(date);
     }
 
-    public static String getInstanceJapanese(){
-        Date date = new Date();
-        return getStringDateJapanese(date);
-    }
-
-    public static String getInstanceFilename(){
-        Date date = new Date();
-        return getStringDateFilename(date);
-    }
-
     public static String getStringDate(Date date){
-        SimpleDateFormat sdf = new SimpleDateFormat(simpleDateFormat);
+        SimpleDateFormat sdf;
+        if(check00(date)){
+            sdf = new SimpleDateFormat(simpleDateFormat_);
+        } else {
+            sdf = new SimpleDateFormat(simpleDateFormat);
+        }
         return sdf.format(date);
     }
 
     public static String getStringDateJapanese(Date date){
-        SimpleDateFormat sdf = new SimpleDateFormat(JapaneseDateFormat);
+        SimpleDateFormat sdf;
+        if (check00(date)){
+            sdf = new SimpleDateFormat(JapaneseDateFormat_);
+        } else {
+            sdf = new SimpleDateFormat(JapaneseDateFormat);
+        }
         return sdf.format(date);
     }
 
     public static String getStringDateFilename(Date date){
-        SimpleDateFormat sdf = new SimpleDateFormat(FileDateFormat);
+        SimpleDateFormat sdf;
+        if (check00(date)){
+            sdf = new SimpleDateFormat(FileDateFormat_);
+        } else {
+            sdf = new SimpleDateFormat(FileDateFormat);
+        }
         return sdf.format(date);
     }
 
     public static String getStringDateFilename2(Date date){
-        SimpleDateFormat sdf = new SimpleDateFormat(FileDateFormat2);
+        SimpleDateFormat sdf;
+        if (check00(date)){
+            sdf = new SimpleDateFormat(FileDateFormat2_);
+        } else {
+            sdf = new SimpleDateFormat(FileDateFormat2);
+        }
         return sdf.format(date);
     }
 
@@ -60,10 +76,8 @@ public class QADateFormat {
         DateFormat df = new SimpleDateFormat(simpleDateFormat);
         Date date = null;
         try {
-            Log.d("Date format", dateFormat);
             date = df.parse(dateFormat);
         } catch (Exception e) {
-            Log.e("Date format", e.toString());
             date = new Date();
         }
         return date;
